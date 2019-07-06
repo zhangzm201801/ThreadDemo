49b16fa5b8e547bc1fa62142676f91b4fad75296
zhangzm201801 <zhangzm1117@126.com>
2019-07-06 15:51:19 +0900

    线程优先级

---
+++ src/com/zzm/thread/TestPriotiry.java
@@ -0,0 +1,47 @@
+package com.zzm.thread;
+
+public class TestPriotiry {
+	public static void main(String[] args) {
+
+		// 主线程默认优先级
+		System.out.println(Thread.currentThread().getName() + "--->" + Thread.currentThread().getPriority());
+
+		MyPriority myPriority = new MyPriority();
+
+		Thread t1 = new Thread(myPriority);
+		Thread t2 = new Thread(myPriority);
+		Thread t3 = new Thread(myPriority);
+		Thread t4 = new Thread(myPriority);
+		Thread t5 = new Thread(myPriority);
+		Thread t6 = new Thread(myPriority);
+
+		// 先设置优先级，再启动
+		t1.start();
+
+		t2.setPriority(1);
+		t2.start();
+
+		t3.setPriority(4);
+		t3.start();
+
+		t4.setPriority(Thread.MAX_PRIORITY);
+		t4.start();
+
+		t5.setPriority(8);
+		t5.start();
+
+		t6.setPriority(9);
+		t6.start();
+
+	}
+}
+
+class MyPriority implements Runnable {
+
+	@Override
+	public void run() {
+		System.out.println(Thread.currentThread().getName() + "--->" + Thread.currentThread().getPriority());
+
+	}
+
+}
